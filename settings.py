57c710ccdea47af1e1f7e5585fb3501902fad3b5
ilia_krylovskiy <krylovskii.ii@phystech.edu>
2021-11-21 21:17:04 +0300

    fixed grammar

--- settings.py
+++ settings.py
@@ -6,11 +6,11 @@ current_skin, spaceship = None, None
 
 # Common classes
 class Button:
-    def __init__(self, x, y, width, heigth, action):
+    def __init__(self, x, y, width, height, action):
         self.x = x
         self.y = y
         self.width = width
-        self.height = heigth
+        self.height = height
         self.action = action
         self.image = None
 
