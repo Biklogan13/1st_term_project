9bf82886e1ffa9c18b7d6052bfd6bb8993ee2fa5
ilia_krylovskiy <krylovskii.ii@phystech.edu>
2021-11-22 23:53:44 +0300

    added speed decay

--- shuttle.py
+++ shuttle.py
@@ -35,6 +35,14 @@ class Shuttle:
         else:
             self.ay = 0
 
+        if self.ay == 0:
+            if self.Vy > 0:
+                self.Vy = -0.1
+            if self.Vy < 0:
+                self.Vy = 0.1
+            if self.Vy < 0.2 and self.Vy > -0.2:
+                self.Vy = 0
+
         if pygame.key.get_pressed()[pygame.K_a]:
             self.ax = -0.2
         elif pygame.key.get_pressed()[pygame.K_d]:
@@ -42,31 +50,32 @@ class Shuttle:
         else:
             self.ax = 0
 
-        if self.ax >= 0:
-            if self.Vx <= 5:
-                self.Vx += self.ax
-        if self.ax <= 0:
-            if self.Vx >= -5:
-                self.Vx += self.ax
-        if self.ay >= 0:
-            if self.Vy <= 5:
-                self.Vy += self.ay
-        if self.ay <= 0:
-            if self.Vy >= -5:
-                self.Vy += self.ay
+        if self.ax == 0:
+            if self.Vx > 0:
+                self.Vx = -0.1
+            if self.Vx < 0:
+                self.Vx = 0.1
+            if self.Vx < 0.2 and self.Vx > -0.2:
+                self.Vx = 0
 
-        if self.Vx >= 0:
-            if self.x <= settings.WIDTH:
-                self.x += self.Vx
-        if self.Vx <= 0:
-            if self.x >= 0:
-                self.x += self.Vx
-        if self.Vy >= 0:
-            if self.y <= settings.HEIGHT:
-                self.y += self.Vy
-        if self.Vy <= 0:
-            if self.y >= 0:
-                self.y += self.Vy
+
+        if self.ax >= 0 and self.Vx <= 5:
+            self.Vx += self.ax
+        if self.ax <= 0 and self.Vx >= -5:
+            self.Vx += self.ax
+        if self.ay >= 0 and self.Vy <= 5:
+            self.Vy += self.ay
+        if self.ay <= 0 and self.Vy >= -5:
+            self.Vy += self.ay
+
+        if self.Vx >= 0 and self.x <= settings.WIDTH:
+            self.x += self.Vx
+        if self.Vx <= 0 and self.x >= 0:
+            self.x += self.Vx
+        if self.Vy >= 0 and self.y <= settings.HEIGHT:
+            self.y += self.Vy
+        if self.Vy <= 0 and self.y >= 0:
+            self.y += self.Vy
 
 def init():
     global screen
