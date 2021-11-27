603c05f4b62d35c60a503845c99e571f34cbd8de
Admin <kozyrev.in@phystech.edu>
2021-11-27 15:22:34 +0300

    Added transparency

--- menu.py
+++ menu.py
@@ -4,22 +4,22 @@ import settings
 
 buttons = []
 screen = None
+background = None
 
 
 def init():
-    global buttons, screen
+    global buttons, screen, background
     screen = pygame.Surface(settings.SIZE)
 
-    background = pygame.image.load('backgrounds/menu_background.jpg').convert_alpha()
+    background = pygame.image.load('backgrounds/menu_background.png').convert_alpha()
     background = pygame.transform.scale(background, settings.SIZE)
-    screen.blit(background, (0, 0))
 
     play_image = pygame.image.load('buttons_images/play_button.png').convert_alpha()
     shop_image = pygame.image.load('buttons_images/shop_button.png').convert_alpha()
     exit_image = pygame.image.load('buttons_images/exit_button.png').convert_alpha()
-    play_image = pygame.transform.scale(play_image, (400, 100)).convert_alpha()
-    shop_image = pygame.transform.scale(shop_image, (400, 100)).convert_alpha()
-    exit_image = pygame.transform.scale(exit_image, (400, 100)).convert_alpha()
+    play_image = pygame.transform.scale(play_image, (400, 100))
+    shop_image = pygame.transform.scale(shop_image, (400, 100))
+    exit_image = pygame.transform.scale(exit_image, (400, 100))
 
     play_button = settings.Button(settings.WIDTH // 2 - 200, settings.HEIGHT // 2 - 200, 400, 100, 'switch_to_levels')
     play_button.image = play_image
@@ -31,7 +31,8 @@ def init():
 
 
 def create_screen():
-    global buttons, screen
+    global buttons, screen, background
+    screen.blit(background, (0, 0))
     for b in buttons:
         b.draw(screen)
     for event in pygame.event.get():
