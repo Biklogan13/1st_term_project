87f771b79cc3aac213fc630466892fcf149b170a
Biklogan13 <krylovskii.ii@phystech.edu>
2021-11-28 17:52:32 +0300

    added kamikazes

--- enemies.py
+++ enemies.py
@@ -1,7 +1,9 @@
 import pygame
 import random
 import settings
+import math
 
+kamikaze_image = None
 mine_image = None
 enemy_counter = 0
 
@@ -30,13 +32,35 @@ class Enemy_heavy:
 class Enemy_kamikaze:
     def __init__(self):
         self.surface = None
-        self.x = 0
-        self.y = 0
+        if random.randint(1, 2) == 1:
+            self.x = -20
+        else:
+            self.x = settings.WIDTH + 20
+        self.y = random.randint(15, settings.HEIGHT - 15)
         self.Vx = 0
         self.Vy = 0
         self.live = 1
-        self.image = None
-        self.r = 0
+        self.image = kamikaze_image
+        self.angle = 0
+        self.r = 22.5
+
+    def draw(self, screen):
+        if self.live == 1:
+            self.image = rot_center(kamikaze_image, self.angle*360/(-2*math.pi) - 90)
+            screen.blit(self.image, (self.x - 15, self.y - 15))
+
+    def move(self):
+        self.angle = math.atan2(settings.spaceship.y - self.y, settings.spaceship.x - self.x)
+        self.Vx = math.cos(self.angle)
+        self.Vy = math.sin(self.angle)
+        self.x += self.Vx
+        self.y += self.Vy
+
+    def hittest(self, obj):
+        if (self.x - obj.x) ** 2 + (self.y - obj.y) ** 2 <= (self.r + obj.r) ** 2:
+            return True
+        else:
+            return False
 
 class Mine:
     def __init__(self):
@@ -63,19 +87,28 @@ class Mine:
             return False
 
 def init():
-    global mine_image
+    global mine_image, kamikaze_image
     mine_image = pygame.image.load('enemy_skins/mine.png').convert_alpha()
     mine_image = pygame.transform.scale(mine_image, (50, 50))
+    kamikaze_image = pygame.image.load('enemy_skins/smalldrone_1.PNG').convert_alpha()
+    kamikaze_image = pygame.transform.scale(kamikaze_image, (30, 45))
 
 def processing(screen):
     global enemy_counter
     if settings.tick_counter % 180 == 0:
         new_mine = Mine()
-        enemy_counter += 1
-        if len(settings.enemies) < 100:
-            settings.enemies.append(new_mine)
-        else:
-            settings.enemies[enemy_counter % 100] = new_mine
+        #nemy_counter += 1
+        #if len(settings.enemies) < 100:
+        settings.enemies.append(new_mine)
+        #else:
+        #settings.enemies[enemy_counter % 100] = new_mine
+    if settings.tick_counter % 360 == 0:
+        new_kamikaze = Enemy_kamikaze()
+        #enemy_counter += 1
+        #if len(settings.enemies) < 100:
+        settings.enemies.append(new_kamikaze)
+        #else:
+        #settings.enemies[enemy_counter % 99] = new_kamikaze
 
     for k in settings.enemies:
         if k.hittest(settings.spaceship):
@@ -88,6 +121,16 @@ def processing(screen):
                 k.live = 0
 
     for k in settings.enemies:
-        k.draw(screen)
         k.move()
+        k.draw(screen)
 
+def rot_center(image, angle):
+    WIDTH = image.get_width()
+    HEIGHT = image.get_height()
+    orig_rect = image.get_rect(width=min(WIDTH, HEIGHT), height=min(WIDTH, HEIGHT))
+    rot_image = pygame.transform.rotate(image, angle)
+    rot_rect = rot_image.get_rect()
+    rot_rect.center = rot_image.get_rect().center
+    print(orig_rect, rot_rect)
+    rot_image = rot_image.subsurface(rot_rect).copy()
+    return rot_image
