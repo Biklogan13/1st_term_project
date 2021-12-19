30a00c3147dc32d883c2197aad1f0224c7db2bda
Admin <kozyrev.in@phystech.edu>
2021-12-19 14:07:32 +0300

    Added sounds to buttons

--- settings.py
+++ settings.py
@@ -14,6 +14,7 @@ current_skin, spaceship = None, None
 enemies, enemy_bullets, coins = [], [], []
 tick_counter = 0
 light_rings, bullets, laser, plasma_balls, ammo, = [], [], None, [], 0
+click_sound = None
 
 # Game settings
 seconds, bullets_firerate, plasma_balls_firerate, bullet_damage, plasma_ball_damage, laser_damage = 0, 10, 60, 20, 100, 1
@@ -33,6 +34,7 @@ RED = 0xFF0000
 YELLOW = 0xFFC91F
 ORANGE = (255, 165, 0)
 
+
 # Common classes
 class Button:
     def __init__(self, x, y, width, height, action):
@@ -48,9 +50,12 @@ class Button:
         self.action = action
 
     def check_mouse(self, event):
-        return event.button == 1 and\
-               0 <= event.pos[0] - self.x <= self.width and\
-               0 <= event.pos[1] - self.y <= self.height
+        check = event.button == 1 and\
+                0 <= event.pos[0] - self.x <= self.width and\
+                0 <= event.pos[1] - self.y <= self.height
+        if check:
+            click_sound.play()
+        return check
 
     def hover_test(self, event):
         if 0 <= event.pos[0] - self.x <= self.width and 0 <= event.pos[1] - self.y <= self.height:
